25e555a29b122740ca3d9449f4caaf1f9727cf40
alexpanmk <alexpanmk@gmail.com>
2023-09-25 12:33:38 +0800

    V1.0 with minimum requirements completed

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tic-tac-toe
 A Simple Tic-Tac-Toe  game
+
+#Features:
+1. Display an empty tic-tac-toe board when the page is initially displayed.
+2. A player can click on the nine cells to make a move.
+3. Every click will alternate between marking an X and O.
+4. Once occupied with an X or O, the cell cannot be played again.
+5. Provided a Reset Game button that will clear the contents of the board.
+
+
+# Known bug
+1. Box occupancy message not reflecting on first click on occupied square
+
+#Future features
+1. Win logic & winning message
+2. Game tie logic
+3. AI player ( to practice writing algorithms / OpenAI player etc )
+
+#To-dos
+1. To clean up / refactor the code / debug the code after Project 1.
--- js/main.js
+++ js/main.js
@@ -1,318 +1,162 @@
-//Heavily inspired by memory-card-game given reference by GA for the pattern used.
-// https://git.generalassemb.ly/sei-sg/SEIF-15-Course-Materials/tree/main/unit1/w04d3/sample-memory-card-game
-
 /*----- constants -----*/
+const icon = {
+    Cross: "/assets/img/cross.svg",
+    Circle: "/assets/img/circle.svg"
+};
+
+// const gameGrid = document.querySelector('.game-grid');
+
+// const boxOccupiedMsg = `Hullo, ${state.playerTurn} please find an empty cell`;
+// const turnMessage = `Hullo, ${state.playerTurn}, your turn`;
 
 
-/*----- state variables -----*/
-// const state = {
-//     gameStarted: false,
-//     gameWon: false,
-//     difficulty: "easy", // easy, medium or hard
-//     flippedCards: 0, // 0, 1 or 2
-//     totalFlips: 0,
-//     totalTime: 0,
-// }
+ /*----- state variables -----*/
+const state = {
+    boardState: [null, null, null, null, null, null, null, null, null],
+    gameStarted: false,
+    gameWon: false,
+    turnCount: 0, //to checkWin @ 5 onwards
+    playerTurn: "",
+    messageBar: "TEST",
+};
 
-
-
-/*
-Sample card object:
-{
-    index, 0,
-    emoji: 🥔,
-    flipped: false,
-    matched: false,
-}
-*/
-let boardState = [];
 
  /*----- cached elements  -----*/
-//  const selectors = {
-//     board: document.querySelector('.board'),
-//     timer: document.querySelector('#timer-second-number'),
-//     moves: document.querySelector('#move-number'),
-//     start: document.querySelector('.start'),
-//     difficulty: document.querySelector('.difficulty'),
-//     win: document.querySelector('.win'),
-//  }
-
+const selectors = {
+    mainDiv: document.getElementById('main') ,
+    gameGrid: document.querySelector('.game-grid'),
+    messageBar: document.querySelector('.message-bar'),
+}
 
  /*----- event listeners -----*/
-// function addEventListeners() {
-//     document.addEventListener('click', event => {
-//         const eventTarget = event.target;
-//         const eventParent = eventTarget.parentElement;
 
-//         if (eventTarget.className.includes('card-front') && !eventParent.className.includes('flipped')) {
-//             handleFlipCard(eventParent.getAttribute('card-index'));
-//             renderNewBoardState();
-//         } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.hasAttribute('disabled')) {
-//             startGame();
-//         } 
-//     });
-
-//     document.addEventListener('change', event => {
-//         const eventTarget = event.target;
-
-//         if (eventTarget.nodeName === 'SELECT' && !state.gameStarted) {
-//             state.difficulty = eventTarget.value;
-//             generateInitialBoardState();
-//             renderInitialBoard();
-//         }
-//     });
-// }
-
+ function addEventListeners(){
+    selectors.mainDiv.addEventListener('click', evt => {
+        if (evt.target.className === "reset-button"){
+            console.log("Resetting...");
+            resetGame();
+        } else {
+            console.log(evt.target.id);
+            tryCell(evt.target.id);
+        };
+    });
+ }
 
  /*----- functions -----*/
- // Helper utility functions
-//  const shuffle = (array) => {
-//     const clonedArray = [...array];
 
-//     for (let index = clonedArray.length - 1; index > 0; index--) {
-//         const randomIndex = Math.floor(Math.random() * (index + 1));
-//         const original = clonedArray[index];
+function advanceTurn(){
 
-//         clonedArray[index] = clonedArray[randomIndex];
-//         clonedArray[randomIndex] = original;
-//     }
+    //checkWin();
+    
+    if (state.playerTurn==="Circle"){
+        state.playerTurn = "Cross";
+        state.messageBar = "Cross's Turn";
+        state.turnCount += 1;
+    } else {
+        state.playerTurn = "Circle";
+        state.messageBar = "Circle's Turn";
+        state.turnCount += 1;   
+    };
 
-//     return clonedArray;
-// }
+    render();
 
-// const pickRandom = (array, items) => {
-//     const clonedArray = [...array];
-//     const randomPicks = [];
+};
 
-//     for (let index = 0; index < items; index++) {
-//         const randomIndex = Math.floor(Math.random() * clonedArray.length);
-        
-//         randomPicks.push(clonedArray[randomIndex]);
-//         clonedArray.splice(randomIndex, 1);
-//     }
+//Check occupancy and commit state
+function tryCell(id){
 
-//     return randomPicks;
-// }
+  console.log("Trying cell...")
+ 
+  if (state.boardState[id-1]==="Cross" || state.boardState[id-1]==="Circle" ){
+    console.log("cell taken");
+    state.messageBar = "Hullo, cell taken"
+    renderMessageBar();
+  } else {
+    //Empty
+    console.log("Commiting turn for " + state.playerTurn);
+    state.boardState[id-1] = state.playerTurn;
 
-// Logic functions
-// function startGame() {
-//     if (state.gameWon) {
-//         // Reset if restarting on victory.
-//         state.flippedCards = 0;
-//         state.totalFlips = 0;
-//         state.totalTime = 0;
-        
-//         generateInitialBoardState();
-//         renderInitialBoard();
-
-//         state.gameWon = false;
-//     }
-
-//     state.gameStarted = true;
-
-//     render();
-
-//     // Start the timer.
-//     // This timer implementation works but its not ideal since its not called exactly every 1000ms.
-//     // use requestAnimationFrame?
-//     state.loop = setInterval(() => {
-//         state.totalTime += 1;
-//         selectors.timer.innerText = state.totalTime;
-//     }, 1000);
+    console.log(state.boardState);
+    advanceTurn();
+  }
+    
 }
 
-function handleFlipCard(cardIndex) {
-    // if (!state.gameStarted) {
-    //     startGame();
-    // }
+//Reset state and render.
+function resetGame(){
+    console.log("Resetting game...");
+    state.boardState = [null, null, null, null, null, null, null, null, null],
+    state.turnCount = 1;
+    state.playerTurn = "Circle";
+    state.gameStarted = true;
 
-    // state.flippedCards += 1;
-    // state.totalFlips += 1;
+    state.messageBar = state.playerTurn + "'s Turn"
 
-    // if (state.flippedCards <= 2) {
-    //     boardState[cardIndex].flipped = true;
-    // }
-
-    // if (state.flippedCards === 2) {
-    //     const flippedCards = boardState.filter(x => x.flipped && !x.matched);
-
-    //     if (flippedCards[0].emoji === flippedCards[1].emoji) {
-    //         flippedCards[0].matched = true;
-    //         flippedCards[1].matched = true;
-    //         state.flippedCards = 0;
-    //     } else {
-    //         // Flip back pair of matching cards.
-    //         setTimeout(() => {
-    //             state.flippedCards = 0;
-    //             flippedCards[0].flipped = false;
-    //             flippedCards[1].flipped = false;
-    //             render();
-    //         }, 1000);
-    //     }
-
-    //     checkWin();
-    // }
-
-    // render();
+    renderStart();
 }
 
-function checkWin() {
-    // If there are no more cards that we can flip, we won the game
-//     const unmatchedCards = boardState.filter(x => !x.matched);
-//     if (unmatchedCards.length === 0) {
-//         setTimeout(() => {
-//             clearInterval(state.loop);
+function renderNewBoardState(){
 
-//             //reactivate buttons, opposite of startGame
-//             state.gameStarted = false;
-//             state.gameWon = true;
+    console.log("Rendering new board state...");
+    //console.log(state.boardState);
 
-//             render();
+    state.boardState.forEach((cellState, index) => {
 
-//         }, 1000);
-//     }
-// }
+        if (cellState !== null){
 
-// function createCard(emoji, cardIndex) {
-//     const cardDiv = document.createElement('div');
-//     cardDiv.classList.add('card');
-//     cardDiv.setAttribute('card-index', cardIndex);
-
-//     const cardFrontDiv = document.createElement('div');
-//     cardFrontDiv.classList.add('card-front');
-//     const cardBackDiv = document.createElement('div');
-//     cardBackDiv.classList.add('card-back');
-
-//     cardBackDiv.innerText = emoji;
-
-//     cardDiv.append(cardFrontDiv, cardBackDiv);
-
-
-//     if (state.difficulty === 'easy') {
-//         cardDiv.classList.add('easy');
-//         cardBackDiv.classList.add('easy');
-//     }
-
-//     return cardDiv;
-// }
-
-function generateInitialBoardState() {
-    const dimensions = dimensionsMap[state.difficulty] || dimensionsMap.easy;
-
-    const picks = pickRandom(possibleEmojis, (dimensions * dimensions) / 2);
-    const items = shuffle([...picks, ...picks]);
-
-    boardState = items.map((x, index) => ({
-        index: index,
-        emoji: x,
-        flipped: false,
-        matched: false,
-    }));
-}
-
-// Board render functions
-function renderInitialBoard () {
-    const dimensions = dimensionsMap[state.difficulty] || dimensionsMap.easy;
-
-    //cleanup in case of restart, such as difficulty change or new game
-    selectors.board.innerHTML = '';
-
-    boardState.forEach((item, index) => {
-        const cardEl = createCard(item.emoji, index, state.difficulty);
-
-        selectors.board.appendChild(cardEl);
+            const cellDiv = document.getElementById(index+1);
+            cellDiv.querySelector('img').src = icon[state.boardState[index]];
+            
+        };
     });
-       
-    selectors.board.style.gridTemplateColumns = `repeat(${dimensions}, auto)`;
+
+    renderMessageBar();
+
 }
 
-// function renderNewBoardState() {
-//     const allCards = document.querySelectorAll('.card');
+//intialise starting grid programmatically
+function renderStart(){
+    
+    //Clear gameGrid
+    selectors.gameGrid.innerHTML="";
+    
+    for (i=0; i<9; i++){
+        let cellDiv = document.createElement('div');
+        let cellImg = document.createElement('img');
+        cellDiv.appendChild(cellImg);
 
-//     allCards.forEach(element => {
-//         const cardData = boardState[element.getAttribute('card-index')];
-
-//         if (cardData.flipped) {
-//             element.classList.add('flipped');
-//         } else {
-//             element.classList.remove('flipped');
-//         }
-
-//         if (cardData.matched) {
-//             element.classList.add('matched');
-//         } else {
-//             element.classList.remove('matched');
-//         }
-//     });
-// }
-
-// function renderMoves() {
-//     selectors.moves.innerText = state.totalFlips;
-// }
-
-// function renderDifficultySelect() {
-//     Object.keys(dimensionsMap).forEach((difficulty) => {
-//         const optionEl = document.createElement('option');
-
-//         optionEl.setAttribute('value', difficulty);
-//         optionEl.innerText = difficulty;
+        cellDiv.className =  "cell";
+        cellDiv.id = i+1;
         
-//         selectors.difficulty.appendChild(optionEl);
-//     });
-// }
+        //console.log(cellDiv.id);
+        selectors.gameGrid.appendChild(cellDiv);
+    }
 
-// function renderStart() {
-//     /*
-//         To set the value of a Boolean attribute, such as disabled, you can specify any value.
-//         An empty string or the name of the attribute are recommended values.
-//         All that matters is that if the attribute is present at all, regardless of its actual value,
-//         its value is considered to be true.
-//     */
-//         selectors.start.setAttribute('disabled', '');
-//         selectors.difficulty.setAttribute('disabled', '');
-//         selectors.start.innerText = 'Game Started';
-// }
+    renderMessageBar();
 
-// function renderGameWon() {
-//     if (state.gameWon) {
-//         confetti();
-
-//         setTimeout(() => {
-//             confetti.reset();
-//         }, 3000);
-//         selectors.win.style.visibility = 'visible';
-//         selectors.win.innerHTML = `
-//             <span class="win-text">
-//                 You won!<br />
-//                 with <span class="highlight">${state.totalFlips}</span> moves<br />
-//                 under <span class="highlight">${state.totalTime}</span> seconds
-//             </span>
-//         `;
-
-//         selectors.start.removeAttribute('disabled');
-//         selectors.difficulty.removeAttribute('disabled');
-//         selectors.start.innerText = 'Restart';
-//     } else {
-//         selectors.win.innerHTML = '';
-//         selectors.win.style.visibility = 'hidden';
-//     }
-
-// }
-
-// function render() {
-//     renderStart();
-//     renderGameWon();
-//     renderNewBoardState();
-//     renderMoves();
-// }
-
-// // Initialize function
-function initialize() {
-    // renderDifficultySelect();
-    generateInitialBoardState();
-    renderInitialBoard();
-    // addEventListeners();
 }
 
+//Message Bar
+function renderMessageBar(){
+    console.log(selectors.messageBar.innerText);
+    selectors.messageBar.innerText = state.messageBar;
+}
 
-initialize();
+function render(){
+    //init empty board if boardState is falsy
+    if(!state.boardState.length && !state.turnCount){
+        renderStart();
+    } else {
+        renderNewBoardState();
+    }
+    renderMessageBar();
+}
+
+function initialise() {
+
+    resetGame();
+    addEventListeners();
+
+}
+
+initialise();
